17809613015b127ee54828789254db090ee1f25a
vagrant <vagrant@homestead>
2018-10-04 09:49:12 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -8,6 +8,14 @@ use Auth;
 
 class SessionController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+
+            'only' => ['create'],
+
+        ]);
+    }
     //
     public function create(){
 
@@ -15,26 +23,20 @@ class SessionController extends Controller
 
     }
 
-    public function store(Request $request){
-
-        $result = $this->validate($request, [
-
-            'email' => 'required|email|max:255',
-            'password' => 'required'
-
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
         ]);
 
-        // dd($request);
-        // exit;
-
-        if (Auth::attempt($result, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来！');
-            return redirect()->route('users.show', [Auth::user()]);
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+           session()->flash('success', '欢迎回来！');
+           return redirect()->intended(route('users.show', [Auth::user()]));
         } else {
-            session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
-            return redirect()->back();
+           session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           return redirect()->back();
         }
-
     }
 
 
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,19 +9,43 @@ use Auth;
 
 class UsersController extends Controller
 {
-    //
+    //初始化方法
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store', 'index']
+        ]);
+
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    public function index(){
+
+        $users = user::paginate(10);
+
+        return view('users.index', compact('users'));
+
+    }
+
+
+    //登录页面
     public function create(){
 
         return view('users.create');
 
     }
 
+
+    //个人信息页面
     public function show(User $user){
 
         return view('users.show', compact('user'));
 
     }
 
+    //注册方法
     public function store(Request $request){
 
         $this->validate($request, [
@@ -38,7 +62,7 @@ class UsersController extends Controller
 
         ]);
 
-
+        //注册成功直接登录
         Auth::login($user);
 
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
@@ -46,5 +70,49 @@ class UsersController extends Controller
 
 
     }
+
+    //修改页面
+    public function edit(User $user){
+
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+
+    }
+
+
+    //修改方法
+    public function update(User $user, Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'password'=> 'required|confirmed|min:6'
+        ]);
+
+        $this->authorize('update', $user);
+
+        $user->update([
+            'name' => $request->name,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+        return redirect()->route('users.show', $user->id);
+
+    }
+
+     public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
+    }
+
 
 }
